e6089704e84da2785681270ab342805e5edec3bd
vidhusan12 <vidhusan709@gmail.com>
2025-09-18 21:58:03 +1200

    add six input boxes for letter guessing and update styles

---
+++ script.js
@@ -0,0 +1,34 @@
+// script.js
+
+const words = ["example", "javascript", "coding", "challenge"];
+let currentWord = "";
+let tries = 0;
+let mistakes = 0;
+
+function scrambleWord(word) {
+  // Scramble and return the scrambled word
+}
+
+function generateRandomWord() {
+  // Generate and display scrambled word
+}
+
+function createInputFields(length) {
+  // Create number of input fields according to the number of letters
+}
+
+function handleInput(event) {
+  // Handle input change event
+}
+
+function resetGame() {
+  // Handle game reset button
+}
+
+document
+  .getElementById("randomButton")
+  .addEventListener("click", generateRandomWord);
+document.getElementById("resetButton").addEventListener("click", resetGame);
+
+// Initial load
+generateRandomWord();
